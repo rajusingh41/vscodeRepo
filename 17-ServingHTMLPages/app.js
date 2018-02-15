14b8efe397b559f62c7d8674ff1441d1133222f4
rajusingh <raju.singh41@gmail.com>
2018-02-16 00:39:34 +0530

    my first

---
+++ 17-ServingHTMLPages/app.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    var myReadStream = fs.createReadStream(__dirname + '/Index.html','utf8');
+    myReadStream.pipe(res);
+
+});
+
+
+server.listen(3002, '127.0.0.1');
+console.log('Open http://localhost:3002/');
