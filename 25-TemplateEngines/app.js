5190c9ab1ac29d1de1a14b09265ba82da48d9a6c
rajusingh <raju.singh41@gmail.com>
2018-02-20 00:17:18 +0530

    Templat

---
+++ 25-TemplateEngines/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+
+    res.sendFile(__dirname + '/index.html');
+
+});
+
+app.get('/contact', function (req, res) {
+    res.sendfile(__dirname + '/contact.html');
+});
+
+
+app.get('/profile/:name', function (req, res) {
+
+    res.render('profile', { names: req.params.name });
+})
+
+
+
+app.listen(3007);
+
+console.log('Open http://localhost:3007');
